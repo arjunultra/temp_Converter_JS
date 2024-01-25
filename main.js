b653c461216492dc4f6dc1467cc40ef4b5dba6f9
Arjun Arasu <arjunbalan1994@gmail.com>
2024-01-25 15:40:41 +0530

    project done

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-let inputFht = Number(document.getElementById("input-fht").value);
-// let resultCel = document.getElementById("result-cel");
-// const convertBtn = document.getElementById("convert-btn");
-// convertBtn.addEventListener("click", function () {
-//   let celcius = ((inputFht - 32) * 5) / 9;
-//   resultCel.innerHTML = celcius.toFixed(2);
-// });
-console.log(inputFht);
+let resultCel = document.getElementById("result-cel");
+const convertBtn = document.getElementById("convert-btn");
+convertBtn.addEventListener("click", function () {
+  let inputFht = Number(document.getElementById("input-fht").value);
+  let celcius = ((inputFht - 32) * 5) / 9;
+  resultCel.innerHTML = celcius.toFixed(2);
+  console.log(inputFht);
+});
