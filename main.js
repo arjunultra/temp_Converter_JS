a53dec707cc23b6e0ebe942f7c20c143187f1f19
Arjun Arasu <arjunbalan1994@gmail.com>
2024-01-27 10:10:44 +0530

    everything is fixed project finished

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 let resultCel = document.getElementById("result-cel");
 const convertBtn = document.getElementById("convert-btn");
-convertBtn.addEventListener("click", function () {
+convertBtn.addEventListener("click", function (e) {
+  e.preventDefault();
   let inputFht = Number(document.getElementById("input-fht").value);
+  console.log(inputFht);
   let celcius = ((inputFht - 32) * 5) / 9;
   resultCel.innerHTML = celcius.toFixed(2);
-  console.log(inputFht);
 });
